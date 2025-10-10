6c328c7fcbe20f6cb16ba810b8d145216b42af29
Kuniyoshi Koji <kuniyoshi.kouji@gmail.com>
2025-10-10 20:31:58 +0900

    add main

---
+++ cmd/ito/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+)
+
+func main() {
+	os.Exit(run(os.Args[1:]))
+}
+
+func run(args []string) int {
+	root, err := defaultRoot()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ito: %v\n", err)
+		return 1
+	}
+
+	if len(args) == 0 || args[0] == "--help" || args[0] == "-h" {
+		printUsage()
+		return 0
+	}
+
+	switch args[0] {
+	case "list":
+		if len(args) > 1 {
+			fmt.Fprintln(os.Stderr, "ito: list は引数を取りません")
+			return 1
+		}
+		if err := listEntries(root); err != nil {
+			fmt.Fprintf(os.Stderr, "ito: %v\n", err)
+			return 1
+		}
+	default:
+		if len(args) > 1 {
+			fmt.Fprintf(os.Stderr, "ito: 不明なサブコマンド: %q\n", args[1])
+			return 1
+		}
+		path, err := resolveEntry(root, args[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ito: %v\n", err)
+			return 1
+		}
+		fmt.Println(path)
+	}
+	return 0
+}
+
+func printUsage() {
+	fmt.Println(`ito は ~/.ito 以下のエントリへ素早くアクセスするためのコマンドです。
+
+使い方:
+  ito list        ~/.ito 内のエントリを列挙します
+  ito <entry>     指定したエントリへの絶対パスを出力します`)
+}
+
+func defaultRoot() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("ホームディレクトリを取得できません: %w", err)
+	}
+	return filepath.Join(home, ".ito"), nil
+}
+
+func listEntries(root string) error {
+	dirEntries, err := os.ReadDir(root)
+	if errors.Is(err, os.ErrNotExist) {
+		return fmt.Errorf("%s が存在しません", root)
+	}
+	if err != nil {
+		return fmt.Errorf("%s を読み取れません: %w", root, err)
+	}
+
+	names := make([]string, 0, len(dirEntries))
+	for _, entry := range dirEntries {
+		names = append(names, entry.Name())
+	}
+	sort.Strings(names)
+
+	for _, name := range names {
+		fmt.Println(name)
+	}
+	return nil
+}
+
+func resolveEntry(root, name string) (string, error) {
+	path := filepath.Join(root, name)
+	if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+		return "", fmt.Errorf("%s は存在しません", path)
+	} else if err != nil {
+		return "", fmt.Errorf("%s を確認できません: %w", path, err)
+	}
+	return path, nil
+}
